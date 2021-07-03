6f58d4b27f19f2cd80988972c1eca583eec56d8e
plally <pierce@vulpes.dev>
2021-07-03 16:24:58 -0400

    Update lua/autorun/server/cfc_destroy_the_gibs.lua
    Co-authored-by: Brandon Sturgeon <sturgeonb4@gmail.com>

--- lua/autorun/server/cfc_destroy_the_gibs.lua
+++ lua/autorun/server/cfc_destroy_the_gibs.lua
@@ -3,14 +3,14 @@ local noGibModels = {
     ["models/props/cs_office/computer_monitor.mdl"] = true
 }
 
-hook.Add("PropBreak", "CFC_NoGibs_RemovePropOnBreak", function(attacker, prop)
-	if not IsValid(prop) then return end
+hook.Add( "PropBreak", "CFC_NoGibs_RemovePropOnBreak", function( attacker, prop )
+    if not IsValid( prop ) then return end
 	
-	local model = prop:GetModel()
-	model = string.Replace(model, "\\", "/")
-	model = string.lower(model)
+    local model = prop:GetModel()
+    model = string.Replace( model, "\\", "/" )
+    model = string.lower( model )
 
-	if noGibModels[model] then
-		prop:Remove()
-	end
+    if not noGibModels[model] then return end
+
+    prop:Remove()
 end)
