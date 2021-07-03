c9fa881b56511e33d45589f249299dfae1faf5b2
pierce <pierce@vulpes.dev>
2021-07-03 12:57:14 -0400

    remove entities for specific models when they break

---
+++ lua/autorun/server/cfc_destroy_the_gibs.lua
@@ -0,0 +1,16 @@
+local noGibModels = {
+	["models/props/cs_office/computer_caseb.mdl"] = true,
+	["models/props/cs_office/computer_monitor.mdl"] = true,	
+}
+
+hook.Add("PropBreak", "CFC_NoGibs_RemovePropOnBreak", function(attacker, prop)
+	if not IsValid(prop) then return end
+	
+	local model = prop:GetModel()
+	model = string.Replace(model, "\\", "/")
+	model = string.lower(model)
+
+	if noGibModels[model] then
+		prop:Remove()
+	end
+end)
